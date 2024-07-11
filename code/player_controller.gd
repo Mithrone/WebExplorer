5edb525af5f44c6a8aa5e0524999b6573ec410a9
Mithrone <zandermarko.vantonder@gmail.com>
2024-07-11 11:03:32 +0200

    Added player movement and collision with static objects and areas

--- code/player_controller.gd
+++ code/player_controller.gd
@@ -1,5 +1,8 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
+var input_delay = 200.0
+var last_input_time = -input_delay
+var step_size = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +10,42 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	if (last_input_time + input_delay < Time.get_ticks_msec()):
+		var _collide
+		if (Input.is_action_pressed("player_1_left")):
+			_collide =  move_and_collide(Vector2(-step_size, 0))
+			last_input_time = Time.get_ticks_msec()
+		if (Input.is_action_pressed("player_1_right")):
+			_collide =  move_and_collide(Vector2(step_size, 0))
+			last_input_time = Time.get_ticks_msec()
+		if (Input.is_action_pressed("player_1_up")):
+			_collide =  move_and_collide(Vector2(0, -step_size))
+			last_input_time = Time.get_ticks_msec()
+		if (Input.is_action_pressed("player_1_down")):
+			_collide =  move_and_collide(Vector2(0, step_size))
+			last_input_time = Time.get_ticks_msec()
+
+		correct_position()
+
+		if (_collide is KinematicCollision2D):
+			print(_collide.get_collider())
+		
 	pass
+
+func correct_position():
+	if (abs(fmod(position.x, step_size)) > (step_size / 2)):
+		position.x = position.x - (abs(fmod(position.x, step_size)) - step_size)
+	else:
+		position.x = position.x - abs(fmod(position.x, step_size))
+
+	if (fmod(position.y, step_size) < 0):
+		if (abs(fmod(position.y, step_size) + step_size) > (step_size / 2)):
+			position.y = position.y - (abs(fmod(position.y, step_size)) - step_size)
+		else:
+			position.y = position.y - abs(fmod(position.y, step_size) + step_size)
+	else:
+		if (abs(fmod(position.y, step_size)) > (step_size / 2)):
+			position.y = position.y - (abs(fmod(position.y, step_size)) - step_size)
+		else:
+			position.y = position.y - abs(fmod(position.y, step_size))
